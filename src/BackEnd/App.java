4ff87e19f05e3e0771f5df6a80b967fb74365e8e
Henrico Swanepoel <henrico@scubed.co.za>
2024-08-19 13:13:33 +0200

    Added basic UI for the application. Next up is functionality.

--- src/BackEnd/App.java
+++ src/BackEnd/App.java
@@ -1,7 +1,45 @@
 package BackEnd;
 
-public class App {
-    public static void main(String[] args) {
+import WindowManagers.Dashboard;
+import WindowManagers.Login;
+import WindowManagers.Registration;
 
+import javax.swing.*;
+
+public class App {
+    private JFrame currentFrame;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            App app = new App();
+            app.showLoginWindow();
+        });
+    }
+
+    public void showLoginWindow() {
+        if (currentFrame != null) {
+            currentFrame.dispose();
+        }
+
+        currentFrame = new Login(this);
+        currentFrame.setVisible(true);
+    }
+
+    public void showRegistrationWindow() {
+        if (currentFrame != null) {
+            currentFrame.dispose();
+        }
+
+        currentFrame = new Registration(this);
+        currentFrame.setVisible(true);
+    }
+
+    public void showDashboardWindow() {
+        if (currentFrame != null) {
+            currentFrame.dispose();
+        }
+
+        currentFrame = new Dashboard(this);
+        currentFrame.setVisible(true);
     }
 }
--- src/WindowManagers/Dashboard.java
+++ src/WindowManagers/Dashboard.java
@@ -1,7 +1,30 @@
 package WindowManagers;
 
-import javax.swing.*;
+import BackEnd.App;
 
-public class Dashboard {
-    private JPanel panel1;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Dashboard extends JFrame {
+    private final App app;
+    private JPanel mainPanel;
+    private JLabel greeting;
+    private JButton logoutButton;
+
+    public Dashboard(App app) {
+        this.app = app;
+        setContentPane(mainPanel);
+        setTitle("Dashboard");
+        setSize(600, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app.showLoginWindow();
+            }
+        });
+    }
 }
